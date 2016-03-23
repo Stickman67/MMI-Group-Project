e349ffe44df2b6a16e7ad43e5273e1e8288f1014
simfrek <simfrek@gmail.com>
2016-03-23 12:33:38 +0000

    Fixed Voice Server

--- Voice Rec Server/Program.cs
+++ Voice Rec Server/Program.cs
@@ -14,7 +14,7 @@ namespace RecoServeur
 	{
 		public static SpeechRecognitionEngine speechRecognitionEngine;
 		public static string reception = "#";
-		public static string endWord = "Stop"; 
+		public static string endWord = "stop"; 
 		public static string ipServer = "127.0.0.1";
 		public static byte[] data = new byte[512];
 		public static int port = 26000;
@@ -34,7 +34,6 @@ namespace RecoServeur
 				// load dictionary
 				try
 				{
-					Choices texts = new Choices();
 					string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\grammar.txt");
 					foreach (string line in lines)
 					{
@@ -73,12 +72,69 @@ namespace RecoServeur
 
 						// skip commentblocks and empty lines..
 						if (line.StartsWith("#") || line == String.Empty) continue;
-
-						texts.Add(line);
 					}
-                    // Edit this to create full commands
-					Grammar wordsList = new Grammar(new GrammarBuilder(texts));
-					speechRecognitionEngine.LoadGrammar(wordsList);
+                    Choices place = new Choices("place", "create", "add");
+                    Choices delete = new Choices("delete", "remove");
+                    Choices all = new Choices("all", "everything");
+                    Choices number = new Choices("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+                    Choices colour = new Choices("red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "white");
+
+                    GrammarBuilder place_no_colour = new GrammarBuilder();
+                    place_no_colour.Append(place);
+                    place_no_colour.Append("marker");
+
+                    GrammarBuilder place_colour = new GrammarBuilder();
+                    place_colour.Append(place);
+                    place_colour.Append(colour);
+                    place_colour.Append("marker");
+
+                    Choices place_cmd = new Choices(place_colour, place_no_colour);
+
+                    GrammarBuilder delete_all_no_colour = new GrammarBuilder();
+                    delete_all_no_colour.Append(delete);
+                    delete_all_no_colour.Append(all);
+
+                    GrammarBuilder delete_all_colour = new GrammarBuilder();
+                    delete_all_colour.Append(delete);
+                    delete_all_colour.Append(all);
+                    delete_all_colour.Append(colour);
+
+                    Choices delete_all_cmd = new Choices(delete_all_no_colour, delete_all_colour);
+
+                    GrammarBuilder delete_no_number = new GrammarBuilder();
+                    delete_no_number.Append(delete);
+                    delete_no_number.Append(colour);
+
+                    GrammarBuilder delete_no_colour = new GrammarBuilder();
+                    delete_no_colour.Append(delete);
+                    delete_no_colour.Append(number);
+
+                    GrammarBuilder delete_both = new GrammarBuilder();
+                    delete_both.Append(delete);
+                    delete_both.Append(colour);
+                    delete_both.Append(number);
+
+                    Choices delete_cmd = new Choices(delete_both, delete_no_colour, delete_no_number);
+
+                    GrammarBuilder goto_no_number = new GrammarBuilder();
+                    goto_no_number.Append("go to");
+                    goto_no_number.Append(colour);
+
+                    GrammarBuilder goto_no_colour = new GrammarBuilder();
+                    goto_no_colour.Append("go to");
+                    goto_no_colour.Append(number);
+
+                    GrammarBuilder goto_both = new GrammarBuilder();
+                    goto_both.Append("go to");
+                    goto_both.Append(colour);
+                    goto_both.Append(number);
+
+                    Choices goto_cmd = new Choices(goto_both, goto_no_colour, goto_no_number);
+
+                    Choices commands = new Choices(place_cmd, delete_cmd, delete_all_cmd, goto_cmd);
+
+					Grammar commandList = new Grammar(new GrammarBuilder(commands));
+                    speechRecognitionEngine.LoadGrammar(commandList);
 				}
 				catch (Exception ex)
 				{
