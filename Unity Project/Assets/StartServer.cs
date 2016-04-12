0fe3039ecc98e7b978521bb564de75e55940fbcf
David Cuthbertson <davidcuthbertson96@gmail.com>
2016-04-12 12:41:22 +0100

    Server Start
    The voice server now launches when the unity project starts so that it
    no longer has to be launched manually

---
+++ Unity Project/Assets/StartServer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using UnityEditor;
+
+public class StartServer : MonoBehaviour {
+
+	static Process server;
+
+	// Use this for initialization
+	void Start () {
+		server = Process.Start (Application.dataPath + "/RecoServeur.exe");
+	}
+
+	void OnApplicationQuit() {
+		server.Close ();
+	}
+}
